b0f277395c1162ce4794bd887df0ca4f9a969eb6
luiza fsc <luizascrs@gmail.com>
2023-09-20 16:15:34 -0300

    mmm

--- codigo/relatorio.php
+++ codigo/relatorio.php
@@ -13,15 +13,15 @@ function enviarEmail($assunto, $mensagem) {
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
-    $mail->Username = 'email';
-    $mail->Password = 'senha';
+    $mail->Username = '@gmail.com';
+    $mail->Password = '';
     $mail->SMTPSecure = 'tls';
     $mail->Port = 587;
 
     // Configurações do e-mail
-    $mail->setFrom('email', 'Equipe FlipIFC');
+    $mail-> setFrom('@gmail.com', 'Equipe FlipIFC');
     
-    $mail->addAddress('destinatario');
+    $mail->addAddress('projetoflipifc@gmail.com');
 
  // Adicione mais endereços, se necessário
     $mail->isHTML(true);
@@ -36,7 +36,6 @@ function enviarEmail($assunto, $mensagem) {
         return "E-mail enviado com sucesso!";
     }
 }
-
 // Função para obter o nome da área pelo código
 function obterNomeDaAreaPeloCodigo($codigo) {
     include('conexao.php');
@@ -96,10 +95,10 @@ function gerarRelatorioSemanal($semana) {
     $mensagem .= "</table>";
 
     // Enviar o e-mail
-    //$resultado = enviarEmail($assunto, $mensagem);
+    $resultado = enviarEmail($assunto, $mensagem);
 
     // Exibir o resultado do envio (pode ser útil para debug, você pode remover ou tratar de outra forma)
-    echo $mensagem;
+    echo $resultado;
 }
 
 // Exemplo de uso - gerar relatório para a semana atual e enviar por e-mail
