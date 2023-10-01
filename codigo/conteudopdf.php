ec221f7d738084ffeb9f977395e39038f7cd6249
raissa <hendlerf.raissa@gmail.com>
2023-10-01 11:02:56 -0300

    relatorio semanal feito automaticamente

--- codigo/conteudopdf.php
+++ codigo/conteudopdf.php
@@ -1,105 +1,63 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f4f4f4;
-            margin: 0;
-            padding: 0;
+<?php
+include('conexao.php');
+
+function obterNomeDaAreaPeloCodigo($codigo) {
+    global $conn;
+
+    $sql = "SELECT nome_area FROM area WHERE codArea = $codigo";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        return $row['nome_area'];
+    } else {
+        return "Área não encontrada";
+    }
+}
+
+function gerarRelatorioSemanal($segunda, $domingo) {
+    global $conn;
+
+    $sql = "SELECT codArea, SUM(resultado) AS acertos, COUNT(*) AS total_questoes FROM resultado WHERE data_hora >= '$segunda' AND data_hora <= '$domingo' GROUP BY codArea";
+    $result = $conn->query($sql);
+
+    $assunto = "Relatório Semanal - $segunda até $domingo";
+    $mensagem = "<h2>Relatório Semanal - $segunda até $domingo</h2>";
+
+    $pontuacoesPorArea = array();
+    while ($row = $result->fetch_assoc()) {
+        $codArea = $row['codArea'];
+        $area = obterNomeDaAreaPeloCodigo($codArea);
+        $acertos = $row['acertos'];
+        $total_questoes = $row['total_questoes'];
+
+        if (!isset($pontuacoesPorArea[$area])) {
+            $pontuacoesPorArea[$area] = array('acertos' => 0, 'total_questoes' => 0);
         }
 
-        h1 {
-            text-align: center;
-            background-color: #007bff;
-            color: #fff;
-            padding: 20px 0;
-        }
-
-        table {
-            width: 80%;
-            margin: 20px auto;
-            border-collapse: collapse;
-            background-color: #fff;
-            box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
-        }
-
-        th, td {
-            padding: 12px 15px;
-            text-align: left;
-            border-bottom: 1px solid #ddd;
-        }
-
-        th {
-            background-color: #007bff;
-            color: #fff;
-        }
-
-        tr:hover {
-            background-color: #f5f5f5;
-        }
-    </style>
-</head>
-<body>
-    <h1>Relatório Semanal</h1>
-
-    <?php
-    include('conexao.php');
-
-    function obterNomeDaAreaPeloCodigo($codigo) {
-        global $conn;
-
-        $sql = "SELECT nome_area FROM area WHERE codArea = $codigo";
-        $result = $conn->query($sql);
-
-        if ($result->num_rows > 0) {
-            $row = $result->fetch_assoc();
-            return $row['nome_area'];
-        } else {
-            return "Área não encontrada";
-        }
+        $pontuacoesPorArea[$area]['acertos'] += $acertos;
+        $pontuacoesPorArea[$area]['total_questoes'] += $total_questoes;
     }
 
-    function gerarRelatorioSemanal($semana) {
-        global $conn;
-
-        $sql = "SELECT codArea, SUM(resultado) AS acertos, COUNT(*) AS total_questoes FROM resultado WHERE WEEK(data_hora) = WEEK('$semana') GROUP BY codArea";
-        $result = $conn->query($sql);
-
-        $assunto = "Relatório Semanal - " . date('Y-m-d', strtotime($semana));
-        $mensagem = "<h2>Relatório Semanal - " . date('Y-m-d', strtotime($semana)) . "</h2>";
-
-        $pontuacoesPorArea = array();
-        while ($row = $result->fetch_assoc()) {
-            $codArea = $row['codArea'];
-            $area = obterNomeDaAreaPeloCodigo($codArea);
-            $acertos = $row['acertos'];
-            $total_questoes = $row['total_questoes'];
-
-            if (!isset($pontuacoesPorArea[$area])) {
-                $pontuacoesPorArea[$area] = array('acertos' => 0, 'total_questoes' => 0);
-            }
-
-            $pontuacoesPorArea[$area]['acertos'] += $acertos;
-            $pontuacoesPorArea[$area]['total_questoes'] += $total_questoes;
-        }
-
-        $mensagem .= "<table>";
-        $mensagem .= "<tr><th>Área</th><th>Acertos</th><th>Total de Questões</th></tr>";
-        foreach ($pontuacoesPorArea as $area => $pontuacoes) {
-            $acertos = $pontuacoes['acertos'];
-            $total_questoes = $pontuacoes['total_questoes'];
-            $mensagem .= "<tr><td>$area</td><td>$acertos</td><td>$total_questoes</td></tr>";
-        }
-        $mensagem .= "</table>";
-
-        echo $mensagem;
+    $mensagem .= "<table>";
+    $mensagem .= "<tr><th>Área</th><th>Acertos</th><th>Total de Questões</th></tr>";
+    foreach ($pontuacoesPorArea as $area => $pontuacoes) {
+        $acertos = $pontuacoes['acertos'];
+        $total_questoes = $pontuacoes['total_questoes'];
+        $mensagem .= "<tr><td>$area</td><td>$acertos</td><td>$total_questoes</td></tr>";
     }
+    $mensagem .= "</table>";
 
-    gerarRelatorioSemanal(date('Y-m-d'));
-    ?>
+    echo $mensagem;
+}
 
-</body>
-</html>
+$segunda_atual = date('Y-m-d', strtotime("next monday"));
+
+$domingo_atual = date('Y-m-d', strtotime("$segunda_atual +6 days"));
+
+$segunda_anterior = date('Y-m-d', strtotime("$segunda_atual -7 days"));
+
+$domingo_anterior = date('Y-m-d', strtotime("$domingo_atual -7 days"));
+
+gerarRelatorioSemanal($segunda_anterior, $domingo_anterior);
+?>
