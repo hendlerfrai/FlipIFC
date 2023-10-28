815cae8d281fd61e63222b5fb65eb3700a4d64c2
ra!ssa <125693037+hendlerfrai@users.noreply.github.com>
2023-10-28 20:17:34 -0300

    Update conexao.php

--- codigo/conexao.php
+++ codigo/conexao.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect('containers-us-west-138.railway.app:6754', 'root', 'OdBCJDun2iBpeeZtWdoq', 'railway');
+$conn = mysqli_connect('', 'root', '', 'railway');
 if(mysqli_connect_errno()) {
     echo "Erro na conexão com o banco de dados";
     exit();
