45125ee5996621f306c15b83799e839153813c51
ra!ssa <125693037+hendlerfrai@users.noreply.github.com>
2023-10-29 09:29:08 -0300

    novo conexao.php

--- codigo/conexao.php
+++ codigo/conexao.php
@@ -1,9 +1,10 @@
+
 <?php
 
-$conn = mysqli_connect('', 'root', '', 'railway');
+$conn = mysqli_connect('monorail.proxy.rlwy.net:28359', 'root', '6dd31gEeBdHE1adBEAg2gC34aFB51E6A', 'railway');
 if(mysqli_connect_errno()) {
     echo "Erro na conexão com o banco de dados";
     exit();
 }
 
-?> 
+?>
