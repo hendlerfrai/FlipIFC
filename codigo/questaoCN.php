5aac6ae846cd98f6f457f680f252d355c98590d4
ra!ssa <125693037+hendlerfrai@users.noreply.github.com>
2023-10-29 13:08:38 -0300

    Update questaoCN.php

--- codigo/questaoCN.php
+++ codigo/questaoCN.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION['codUser'])) {
     exit();
 }
 
-$_SESSION['escolhaAnterior'] = "questaoLP";
+$_SESSION['escolhaAnterior'] = "questaoCN";
 $dataAtual = date("Y-m-d");
 $data_hora = date("Y-m-d H:i:s");
 
