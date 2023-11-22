c20209586cd6fa34a1af997f0c9fe761ddce65a7
ra!ssa <125693037+hendlerfrai@users.noreply.github.com>
2023-11-22 10:42:01 -0300

    Update cadastro.php

--- codigo/cadastro.php
+++ codigo/cadastro.php
@@ -1,104 +1,9 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+<?php
+include "conexao.php";
+?>
 
-        <?php
-        include "conexao.php";
-        ?>
-<style>
-    /* Reset de estilos para remover margens e preenchimentos padrão */
-* {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-}
-
-/* Defina uma fonte de backup em caso de falha no carregamento da fonte principal */
-body {
-    font-family: 'Poppins', sans-serif;
-}
-
-/* Estilo para o título principal */
-h1 {
-    font-size: 36px;
-    text-align: center;
-    margin-top: 20px;
-    color: #333;
-}
-
-/* Estilo para o formulário de criação de conta */
-.formulario {
-    max-width: 400px;
-    margin: 0 auto;
-    padding: 20px;
-    background-color: #f5f5f5;
-    border: 1px solid #ddd;
-    border-radius: 5px;
-}
-
-.formulario h3 {
-    font-size: 24px;
-    margin-bottom: 20px;
-    color: #333;
-}
-
-.formulario label {
-    display: block;
-    margin-bottom: 10px;
-    color: #555;
-}
-
-.formulario input[type="text"],
-.formulario select {
-    width: 100%;
-    padding: 10px;
-    margin-bottom: 20px;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-}
-
-.formulario select {
-    appearance: none;
-    -webkit-appearance: none;
-    -moz-appearance: none;
-    background-image: url('https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css');
-    background-repeat: no-repeat;
-    background-position: right center;
-    background-size: 16px;
-}
-
-/* Estilo para o botão de cadastro */
-#jogo input[type="submit"] {
-    background-color: #007BFF;
-    color: #fff;
-    border: none;
-    padding: 10px 20px;
-    border-radius: 5px;
-    cursor: pointer;
-}
-
-#jogo input[type="submit"]:hover {
-    background-color: #0056b3;
-}
-
-/* Estilo para o botão "já tenho uma conta" */
-.login button {
-    background-color: transparent;
-    border: none;
-    padding: 10px;
-    margin-top: 20px;
-    cursor: pointer;
-}
-
-.login a {
-    text-decoration: none;
-    color: #007BFF;
-}
-
-.login button:hover a {
-    text-decoration: underline;
-}
-
-</style>
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -106,79 +11,163 @@ h1 {
     <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" />
     <link rel="stylesheet" type="text/css" href="stylecadastro.css">
-    <title> cadastro </title>
+    <style>
+        body {
+            background: radial-gradient(circle at 50% 50%, #e4e0d7, #cf3e6f, #5a54c5);
+            background-size: cover;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .formulario {
+            width: 420px;
+            height: auto;
+            border-radius: 15px;
+            background-color: rgb(216, 216, 216);
+            box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+            transition: 0.4s ease-in-out;
+            margin: auto;
+            padding: 20px;
+            text-align: center;
+        }
+
+        .formulario:hover {
+            box-shadow: 1px 1px 1px rgba(0, 0, 0, 0.1);
+            transform: scale(0.99);
+        }
+
+        .conta {
+            position: relative;
+        }
+
+        h3 {
+            color: black;
+            font-weight: bold;
+            margin-top: 0;
+        }
+
+        label {
+            display: block;
+            margin-bottom: 8px;
+        }
+
+        input,
+        select {
+            width: 100%;
+            padding: 8px;
+            margin-bottom: 16px;
+            box-sizing: border-box;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+        }
+
+        .login {
+            text-align: center;
+        }
+
+        .login button {
+            background-color: #4CAF50;
+            border: none;
+            color: white;
+            padding: 10px 20px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            margin: 4px 2px;
+            cursor: pointer;
+            border-radius: 4px;
+            transition: 0.4s ease-in-out;
+        }
+
+        .login button:hover {
+            background-color: #45a049;
+        }
+
+        .btn1 {
+            position: relative;
+  width: 17em;
+  height: 3em;
+  border-radius: 5px;
+  border: none;
+  outline: none;
+  transition: 0.4s ease-in-out;
+  box-shadow: 1px 1px 3px #b5b5b5, -1px -1px 3px #ffffff;}
+
+        .btn1:active {
+            box-shadow: inset 3px 3px 6px #b5b5b5, inset -3px -3px 6px #ffffff;
+        }
+
+        .btn1:hover,
+        .btn1:focus {
+            background-color: #45a049;
+        }
+    </style>
 </head>
 
 <body>
-        <h1> FlipIFC </h1>
     <div class="formulario">
+        <form method="POST" action="cadastroBack.php" class="conta" id="formCadastro">
+            <h3>Criar uma conta</h3>
 
-    <form method="POST" action="cadastroBack.php" class="conta">
+            <label for="nome">Nome completo:</label>
+            <input type="text" id="nome" name="nome" required>
 
-            <h3>criar uma conta</h3>
-            <label for="nome"> nome completo: </label>
-            <input type="text" id="nome" name="nome">
-            <br>
-            </br>
+            <label for="cod">Código:</label>
+            <input type="password" id="cod" name="cod" maxlength="5" required>
 
-            <label for="cod"> código :</label>
-            <input type="text" id="cod" name="cod">
-            <br> <!-- tirar -->
-            </br>
-
-            <label for="escola"> instituição:</label>
-            <select name="escola">
-                <option>Selecione</option>
+            <label for="escola">Campus:</label>
+            <select name="escola" value="Selecione">
+                <option hidden>Selecione</option>
                 <?php
                 $query = "SELECT * FROM escola";
                 $result = $conn->query($query);
                 while ($row = $result->fetch_array()) {
-                    echo '<option value="'. $row['codEscola']. '">'. $row['nomeEscola']. '</option>';
+                    echo '<option value="' . $row['codEscola'] . '">' . $row['nomeEscola'] . '</option>';
                 }
                 ?>
             </select>
-         <br>
-        </br>
-            <label for="turma"> turma:</label>
-            <select name="turma">
-                <option>Selecione</option>
-                <?php
-                $query = "SELECT * FROM turma";
-                $result = $conn->query($query);
-                while ($row = $result->fetch_array()) {
-                    echo '<option value="'. $row['codTurma']. '">'. $row['turma']. '</option>';
-                }
-                ?>
-            </select>
-        <br>
-        </br>
 
-        <!-- banco de dados -->
-            <label for="cidade"> cidade:</label>
-            <select name="cidade">
-                <option>Selecione</option>
-                <?php
-                $query = "SELECT * FROM cidade";
-                $result = $conn->query($query);
-                while ($row = $result->fetch_array()) {
-                    echo '<option value="'. $row['codCidade']. '">'. $row['nomeCidade']. '</option>';
-                }
-                ?>
-            </select>
-        <br>
-        </br>
-       <!-- fim do banco de dados -->
-
-            <div class="col-6 col-md-4" id="jogo">
-                <input  type="submit" name="submit" value="Cadastrar">
+            <div class="btn" id="jogo">
+                <input type="hidden" name="submit" value="Cadastrar">
+                <a href="areas.php"></a>
             </div>
 
+            <button class="btn1" type="submit">Cadastrar</button>
+
             <div class="login">
-                <button class="login">
-                    <a href="login.php" target="_blank" style="text-decoration: none;"> já tenho uma conta </a>
+                <button class="login" onclick="window.location.href='telaloginUsuario.php'">
+                    Já tenho uma conta
                 </button>
             </div>
         </form>
     </div>
+
+    <script>
+        const form = document.getElementById('formCadastro');
+        let currentInputIndex = 0;
+
+        document.addEventListener('keydown', function (e) {
+            if (e.key === 'ArrowDown') {
+                currentInputIndex = (currentInputIndex + 1) % form.elements.length;
+                form.elements[currentInputIndex].focus();
+            } else if (e.key === 'ArrowUp') {
+                currentInputIndex = (currentInputIndex - 1 + form.elements.length) % form.elements.length;
+                form.elements[currentInputIndex].focus();
+            } else if (e.key === 'Enter') {
+                currentInputIndex = (currentInputIndex - 1 + form.elements.length) % form.elements.length;
+                form.elements[currentInputIndex].focus();
+
+                // Se for o último input, submit o formulário
+                if (currentInputIndex === form.elements.length - 1) {
+                    form.submit();
+                }
+            }
+        });
+    </script>
 </body>
+
 </html>
